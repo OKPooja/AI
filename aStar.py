ca0209b78cc7768509c47b66c01fe8865ae8488b
Pooja Gawade <104082200+OKPooja@users.noreply.github.com>
2024-05-02 01:05:41 +0530

    Update aStar.py

--- aStar.py
+++ aStar.py
@@ -1,74 +1,76 @@
 from queue import PriorityQueue
 
 def aStar(graph, curr_node, goal_node, visited, open_list, closed_list, heuristic, cost):
-    visited.add(curr_node)
-    open_list.append(curr_node)
+    open_list.add(curr_node)
     q = PriorityQueue()
     q.put((0, 0, curr_node))
     parent = {}
     parent[1] = 0
+    distance = {node: float('inf') for node in heuristic}
+    distance[curr_node] = 0
 
     while not q.empty():
         
         total_cost, path_cost, top = q.get()
-        open_list.remove(top)
-        closed_list.append(top)
-        print(f'Current node: {top}', end= " ")
+        if top in open_list:
+            open_list.remove(top)
+        closed_list.add(top)
+        print(f'Current node: {top}')
         print(f'Path cost: {path_cost}')
+        print(f'Total cost : {total_cost}')
         print("Open list:", open_list)
         print("Closed list:", closed_list)
+        print()
         if top == goal_node:
+            print(distance)
             return parent
             return
         
         for node in graph[top]:
-            if node not in visited:
-                open_list.append(node)
-                visited.add(node)
-                parent[node] = top
-                if(top > node):
-                    c = cost[(node, top)]
-                else: 
-                    c = cost[(top, node)]
-                 
-                h = heuristic[node]
+            
+            open_list.add(node)
+            parent[node] = top
+            if(top > node):
+                c = cost[(node, top)]
+            else: 
+                c = cost[(top, node)]
+                
+            h = heuristic[node]
+            new_distance = path_cost + c + h
+            if(new_distance < distance[node]) : 
                 q.put((path_cost + c + h, path_cost + c, node))
+                distance[node] = new_distance
     
 if __name__ == "__main__":
+
+
     graph = {
-        1: [2, 3],
-        2: [1, 5, 6],
-        3: [1, 4, 5],
-        4: [3, 5],
-        5: [2,3,4,7],
-        6: [2, 7],
-        7: [5, 6]
+    0: [1, 2],
+    1: [2, 3, 4],
+    2: [3],
+    3: [4],
     }
     heuristic = {
-        1: 14,
-        2: 12,
-        3: 11,
-        4: 6,
-        5: 4,
-        6: 11,
-        7: 0
+        0: 7,
+        1: 6,
+        2: 2,
+        3: 1,
+        4: 0,
     }
     cost = {
-        (1, 2): 4,
-        (1, 3): 3,
-        (2, 5): 12,
-        (2, 6): 5,
-        (3, 4): 7,
-        (4, 5): 2,
-        (3, 5): 10,
-        (5, 7): 5,
-        (6, 7): 16,
+        (0, 1): 1,
+        (0, 2): 4,
+        (1, 2): 2,
+        (1, 3): 5,
+        (1, 4): 12, 
+        (2, 3): 2,
+        (3, 4): 3,
     }
-    start_node = 1
-    goal_node = 7
+    start_node = 0
+    goal_node = 4
     visited = set()
-    open_list = []
-    closed_list = []
+    open_list = set()
+    closed_list = set()
     parent = aStar(graph, start_node, goal_node, visited, open_list, closed_list,heuristic, cost)
     path = []
     path.append(goal_node)
